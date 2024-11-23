40610c706402769fc1390ddae2ece7f848c5fc43
Tim <timothe.jacquot@epitech.eu>
2024-11-23 02:33:46 +0100

    feat(webapp): Create sveltekit PoC

---
+++ webapp/sveltekit/src/lib/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let overall = false;
+</script>
+
+<div class="relative flex items-center rounded-full text-secondary">
+    <input
+            id="checkbox-all-search"
+            type="checkbox"
+            value=""
+            class="peer w-5 h-5 relative z-10 rounded-md appearance-none cursor-pointer border-neutral-300 checked:bg-center border-2 checked:border-secondary checked:bg-secondary checked:bg-opacity-20">
+    <label for="checkbox-all-search" class="sr-only">checkbox</label>
+    {#if overall}
+        <i class="fi fi-br-minus hidden absolute inset-0 items-center justify-center peer-checked:flex text-xs"></i>
+    {:else}
+        <i class="fi fi-br-check hidden absolute inset-0 items-center justify-center peer-checked:flex text-xs"></i>
+    {/if}
+</div>
---
+++ webapp/sveltekit/src/routes/signup/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Input from "$lib/Input.svelte";
+    import Submit from "$lib/Submit.svelte";
+    import Validation from "$lib/Validation.svelte";
+</script>
+
+<Input title="Username" placeholder="Enter your username" dataType="text" />
+<Input title="Email" placeholder="Enter your email" dataType="email" />
+<Input title="Password" placeholder="Create your password" dataType="password" />
+<Validation description="Must be at least 8 characters"/>
+<Validation description="Must contain one special character"/>
+<Submit text="Get started"/>
+<p>
+    Already have an account?<a href="/login/" class="font-semibold pl-2">Log in</a>
+</p>
